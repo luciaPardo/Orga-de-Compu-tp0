1ab4cb8e0e349d0fdfea843ca2da3931865cf232
germanlahitte <49814867+germanlahitte@users.noreply.github.com>
2020-11-10 21:35:39 -0300

    Add files via upload
    piso

--- valor64.c
+++ valor64.c
@@ -10,7 +10,7 @@ char valor64 (int alDecimal){
 	char caracterEquivalente;
 	char codigo[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-	if (alDecimal<63){
+	if ((alDecimal > -1) && (alDecimal<64)){
 		caracterEquivalente = codigo[alDecimal];
 	} else {
 		caracterEquivalente ='\0';
